9682ad855980c1dd757ef5c5499cba9e270d0655
AnxiousForLife <icolthar@trinity.edu>
2016-05-15 13:08:48 -0500

    Created Sandbag object; added dummy bg music.

---
+++ src/main/scala/game/Sandbag.scala
@@ -0,0 +1,19 @@
+package game
+
+abstract class Sandbag(override val noun: String,
+              override val art: String,
+              override val adj: Option[String],
+              location: Container,
+              capacity: Int) extends Item(noun, art, adj, location) {
+  var amt: Int
+
+  def remaining = capacity - amt
+  def fill = { amt = capacity }
+  def empty = { amt = 0 }
+  def receiveAmt(x: Int) = { amt = capacity.min(x)}
+  def pourInto(bag2: Sandbag) = {
+    val diff = amt.min(bag2.remaining)
+    amt -= diff
+    bag2.amt += diff
+  }
+}
--- src/main/scala/game/main/Main.scala
+++ src/main/scala/game/main/Main.scala
@@ -1,12 +1,11 @@
 package game.main
 
-import scala.collection.mutable
-
-import game.assets._
 import game.Direction._
+import game.{GameState, Item}
+import game.assets._
 import game.engine.Engine
-import game.Item
-import game.GameState
+
+import scala.collection.mutable
 
 object Main {
   def main(args: Array[String]) {
@@ -16,6 +15,23 @@ object Main {
     val startState = new GameState(Seq((startRoom, startDirection)), startRoom, startDirection, startInventory)
 
     val engine = new Engine(startState)
+
+    import java.io._
+    import sun.audio._
+    import javax.sound.sampled.AudioSystem
+    import javax.sound.sampled.Clip
+
+    val file = "/Users/icoltharp/IdeaProjects/timelike/src/main/scala/files/music.wav"
+
+    val in = new FileInputStream(file)
+    //val audioStream = new AudioStream(in)
+    //AudioPlayer.player.start(audioStream)
+
+    val clip = AudioSystem.getClip()
+    val inputStream = AudioSystem.getAudioInputStream(new File("/Users/icoltharp/IdeaProjects/timelike/src/main/scala/files/music.wav"))
+
+    clip.open(inputStream)
+    clip.loop(Clip.LOOP_CONTINUOUSLY)
 
     engine.gameLoop()
   }
